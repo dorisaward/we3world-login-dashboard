21011269778186bcf43a1c3e1a79adb0b1965928
dorisaward <dorisaward@gmail.com>
2023-12-15 16:03:36 +0000

    add react native testing library

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,16 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {it, expect} from '@jest/globals';
+import {render} from '@testing-library/react-native';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  // Given
+  const renderable = <App />;
+
+  // When
+  const {toJSON} = render(renderable);
+
+  // Then
+  expect(toJSON()).toMatchSnapshot();
 });
